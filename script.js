b16acd0e8e8577f6e3601cbe420d1ff5c15c7587
Puthin Krishna <107943187+Puthin7778@users.noreply.github.com>
2024-04-26 14:53:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('downloadBtn').addEventListener('click', function() {
+    // Set the URL of your PDF certificate
+    var pdfUrl = 'AWS Certified Solutions Architect- Associate certificate.pdf';
+    // Open the PDF in a new tab/window
+    window.open(pdfUrl, '_blank');
+});
